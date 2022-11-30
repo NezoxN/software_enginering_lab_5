02837eb256cc518cf6f1044d0560a8392526367d
NezoxN <kovalnikita252@gmail.com>
2022-11-30 18:10:54 +0200

    false unit

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -6,122 +6,77 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
 {
-	TEST_CLASS(UnitTest_true)
+	TEST_CLASS(UnitTest_false) {
+
+public:
+
+	TEST_METHOD(TestMethod9_false)
 	{
-	public:
+		double n = 6;
+		double x = 2;
+		double expected = 18984;
 
-		TEST_METHOD(TestMethod1_true)
-		{
-			int n = 3;
-			int x = 1;
-			int expected = 240;
+		double actual = calc(x, n);
 
-			int actual = calc(x, n);
+		Assert::AreEqual(expected, actual);
+	}
+public:
 
-			Assert::AreEqual(expected, actual);
-		}
+	TEST_METHOD(TestMethod10_false)
+	{
+		double n = 4;
+		double x = 1;
+		double expected = 84;
 
-	public:
+		double actual = calc(x, n);
 
-		TEST_METHOD(TestMethod2_true)
-		{
-			double n = 3;
-			double x = 2;
-			double expected = 420;
+		Assert::AreEqual(expected, actual);
+	}
+public:
 
-			double actual = calc(x, n);
+	TEST_METHOD(TestMethod11_false)
+	{
+		double n = 1;
+		double x = 2;
+		double expected = 1989;
 
-			Assert::AreEqual(expected, actual);
-		}
+		double actual = calc(x, n);
 
-	public:
+		Assert::AreEqual(expected, actual);
+	}
 
-		TEST_METHOD(TestMethod3_true)
-		{
-			double n = 3;
-			double x = 3;
-			double expected = 684;
-
-			double actual = calc(x, n);
-
-			Assert::AreEqual(expected, actual);
-		}
-
-	public:
-
-		TEST_METHOD(TestMethod4_true)
-		{
-			double n = 4;
-			double x = -1;
-			double expected = 4;
-
-			double actual = calc(x, n);
-
-			Assert::AreEqual(expected, actual);
-		}
-
-	public:
-
-		TEST_METHOD(TestMethod5_true)
-		{
-			double n = 4;
-			double x = 5;
-			double expected = 73704;
-
-			double actual = calc(x, n);
-
-			Assert::AreEqual(expected, actual);
-		}
 	};
-	TEST_CLASS(checkValidParams_Tests_true)
+	TEST_CLASS(checkValidParams_Tests_false) {
+public:
+	TEST_METHOD(TestMethod12_false)
 	{
-	public:
+		double h = 12;
 
-		TEST_METHOD(TestMethod6_true)
+		try
 		{
-			double h = 2;
-
-			try
-			{
-				checkValidParams_h(h);
-				Assert::IsTrue(true);
-			}
-			catch (...)
-			{
-				Assert::Fail();
-			}
+			checkValidParams_h(h);
+			Assert::Fail();
 		}
-
-	public:
-		TEST_METHOD(TestMethod7_true)
+		catch (...)
 		{
-			double h = -5;
-
-			try
-			{
-				checkValidParams_h(h);
-				Assert::Fail();
-			}
-			catch (...)
-			{
-				Assert::IsTrue(true);
-			}
+			Assert::IsTrue(true);
 		}
+	}
 
-	public:
-		TEST_METHOD(TestMethod8_true)
+public:
+	TEST_METHOD(TestMethod13_false)
+	{
+		double n = 3;
+
+		try
 		{
-			double n = 1;
-
-			try
-			{
-				checkValidParams_n(n);
-				Assert::Fail();
-			}
-			catch (...)
-			{
-				Assert::IsTrue(true);
-			}
+			checkValidParams_n(n);
+			Assert::IsTrue(true);
 		}
+		catch (...)
+		{
+			Assert::Fail();
+		}
+	}
 	};
 }
